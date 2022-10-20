ceb8de083ac5acf64c34d25aea7215364397e56d
Renan Rocha <renansouza2310@gmail.com>
2022-10-19 21:38:07 -0300

    Alterações na classe CustomerController

--- src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
+++ src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -13,15 +13,22 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/customers")
 class CustomerController {
+    val customers = mutableListOf<CustomerModel>()
 
     @GetMapping
-    fun getCustomer(): CustomerModel {
-        return CustomerModel(1L, "Renan", "email@email.com")
+    fun getCustomer(): List<CustomerModel> {
+        return customers
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     fun create(@RequestBody customer: PostCustomerRequest) {
-        println(customer)
+        var id = if (customers.isEmpty()) {
+            1
+        } else {
+            customers.last().id + 1
+        }
+
+        customers.add(CustomerModel(id, customer.name, customer.email))
     }
 }
