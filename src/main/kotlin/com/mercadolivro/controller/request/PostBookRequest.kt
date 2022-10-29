86a475c5ae247726a72b5261fad0a655f504604e
Renan Rocha <renansouza2310@gmail.com>
2022-10-28 23:38:37 -0300

    Correções no PostBookRequest.

--- src/main/kotlin/com/mercadolivro/controller/request/PostBookRequest.kt
+++ src/main/kotlin/com/mercadolivro/controller/request/PostBookRequest.kt
@@ -2,14 +2,15 @@ package com.mercadolivro.controller.request
 
 import com.fasterxml.jackson.annotation.JsonAlias
 import java.math.BigDecimal
-import javax.validation.constraints.Email
+import javax.validation.constraints.NotEmpty
+import javax.validation.constraints.NotNull
 
 data class PostBookRequest(
 
-    @field:Email(message = "Name must be filled.")
+    @field:NotEmpty(message = "Name must be filled.")
     var name: String,
 
-    @field:Email(message = "Price must be filled.")
+    @field:NotNull(message = "Price must be filled.")
     var price: BigDecimal,
 
     @JsonAlias("customer_id") // como customer_id é diferente de customerId, essa anotação faz um parse para indicar que os dois são a mesma coisa para o spring
